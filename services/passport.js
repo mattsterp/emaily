33af79f48a6fe089b7fb3e8e1a780d371a0b4574
Matthew Peters <mattsterp@gmail.com>
2019-08-21 13:27:38 -0400

    cookie serialization.

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done)=> {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id,done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -9,10 +23,19 @@ passport.use(
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, 
-(accessToken, refreshToken, profile, done) => {
-  console.log('accessToken',accessToken);
-  console.log('refreshToken',refreshToken);
-  console.log('profile', profile);
-})
+    (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id }).then((existingUser) => {
+            if(existingUser) {
+                // We already have a record wiht the given profile ID
+                done(null, existingUser);
+            } else {
+                // We dont have a user record wiht this ID make, a new record
+                new User({ googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+            }
+    });
+    }
+  )
 );
 

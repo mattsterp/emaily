02d33b90810b55d692671c9a6a5989b1f0baf4cd
Matthew Peters <mattsterp@gmail.com>
2019-08-24 15:44:06 -0400

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo"/>
+      <p>
+        hello from react app!
+      </p>
+      <a href="/auth/google">Sign in with Google</a>
+    </header>
+  </div>;
+}
+
+export default App;

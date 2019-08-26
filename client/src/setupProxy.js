dde77588d598670bfd73ba5dcc25e175b2525b19
Matthew Peters <mattsterp@gmail.com>
2019-08-26 13:15:25 -0400

    proxy middle-ware function

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
+//Transform relative link from front-end server (like /auth/google)to back-end server
 const proxy = require('http-proxy-middleware');
 
-module.exports = function(app) {
-    app.use(proxy(['api/', 'auth/google'], {
-        target: 'http://localhost:5000'
-    }));
-}
+module.exports = function (app) {
+    app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
+    app.use(proxy('/api/**', { target: 'http://localhost:5000' }));
+};

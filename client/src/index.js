f86082440ee7addafe6ccf52f6b6bf00bb058f57
Matthew Peters <mattsterp@gmail.com>
2019-09-03 14:35:18 -0400

    import redux

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from "redux";
+
+import App from './components/App';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
